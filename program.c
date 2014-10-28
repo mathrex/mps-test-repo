bdddcfe2808438f490c498f01ce30bb9faf923e3
iuliap <iulia.paraicu@cti.pub.ro>
2014-10-28 17:38:21 +0200

    functie inmul

--- program.c
+++ program.c
@@ -1,2 +1,21 @@
 #include<stdio.h>
 
+void inmul(int first[10][10], int second[10][10], int multiply[10][10])
+{
+  int m, n, p, q, c, d, k, sum = 0;
+  m = p = q = 10;
+ 
+    for ( c = 0 ; c < m ; c++ )
+    {
+      for ( d = 0 ; d < q ; d++ )
+      {
+        for ( k = 0 ; k < p ; k++ )
+        {
+          sum = sum + first[c][k]*second[k][d];
+        }
+ 
+        multiply[c][d] = sum;
+        sum = 0;
+      }
+    }
+}
